e1dd8ca753fa4262978a9561246f719dfb87a460
rupali186 <35391407+rupali186@users.noreply.github.com>
2019-02-22 17:50:06 +0530

    delete/todos/id added

--- server/server.js
+++ server/server.js
@@ -49,6 +49,24 @@ app.get('/todos/:id',(req,res)=>{
 	});
 	//res.send(req.params);
 });
+
+app.delete('/todos/:id',(req,res)=>{
+	var id=req.params.id;
+	if(!ObjectID.isValid(id)){
+		res.status(400).send({});
+		return console.log('ID is invalid');
+	};
+	Todo.findByIdAndRemove(id).then((todo)=>{
+		if(!todo){
+			res.status(404).send({});
+		}else{
+			res.status(200).send({todo});
+		}
+		console.log(todo);
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
 app.listen(port,()=>{
 	console.log(`Started on port ${port}`);
 });
