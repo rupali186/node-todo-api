4ee4d2ed32e415d470ffdace588b9ed95dcb2c52
rupali186 <35391407+rupali186@users.noreply.github.com>
2019-02-22 11:58:51 +0530

    added GET/todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+const {ObjectID}=require('mongodb');
 
 var {mongoose}=require('./db/mongoose.js');
 var {Todo}=require('./models/todo.js');
@@ -29,6 +30,24 @@ app.get('/todos',(req,res)=>{
 	},(e)=>{
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id',(req,res)=>{
+	var id=req.params.id;
+	if(!ObjectID.isValid(id)){
+		res.status(400).send({});
+		return console.log('ID is invalid');
+	}
+	Todo.findById(id).then((todo)=>{
+		if(todo){
+			res.send({todo});
+		}else{
+			res.status(404).send({});
+		}
+	},(e)=>{
+		res.status(400).send(e);
+	});
+	//res.send(req.params);
 });
 app.listen(port,()=>{
 	console.log(`Started on port ${port}`);
