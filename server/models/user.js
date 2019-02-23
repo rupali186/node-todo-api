17303c6d9eb17a400cbdc7adf13a2bd54874adf8
rupali186 <35391407+rupali186@users.noreply.github.com>
2019-02-23 21:43:52 +0530

    POST/users/login route added

--- server/models/user.js
+++ server/models/user.js
@@ -45,7 +45,8 @@ UserSchema.methods.generateAuthToken=function(){
 	return user.save().then(()=>{
 		return token;
 	});
-}
+};
+
 UserSchema.statics.findByToken=function(token){
 	var User=this;
 	var decoded;
@@ -62,6 +63,26 @@ UserSchema.statics.findByToken=function(token){
 		'tokens.token':token,
 		'tokens.access':'auth'
 	});
+};
+
+UserSchema.statics.findByCredentials=function(email,password){
+	var User=this;
+	return User.findOne({email}).then((user)=>{
+		if(!user){
+			return Promise.reject();
+		}
+		return new Promise((resolve,reject)=>{
+			bcrypt.compare(password,user.password,(err,res)=>{
+				if(res){
+					resolve(user);
+				}else{
+					reject();
+				}
+			});
+		});
+		
+	});
+
 };
 
 UserSchema.pre('save',function(next){
