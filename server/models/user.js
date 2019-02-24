18494a28031aa163f74263e985d3149a7d51b403
rupali186 <35391407+rupali186@users.noreply.github.com>
2019-02-24 18:43:10 +0530

    update configuration

--- server/models/user.js
+++ server/models/user.js
@@ -40,7 +40,7 @@ UserSchema.methods.toJSON=function(){
 UserSchema.methods.generateAuthToken=function(){
 	var user=this;
 	var access='auth';
-	var token=jwt.sign({_id:user._id.toHexString(),access},'123abc').toString();
+	var token=jwt.sign({_id:user._id.toHexString(),access},process.env.JWT_SECRET).toString();
 	user.tokens=user.tokens.concat([{access,token}]);
 	return user.save().then(()=>{
 		return token;
@@ -69,7 +69,7 @@ UserSchema.statics.findByToken=function(token){
 	var User=this;
 	var decoded;
 	try{
-		decoded=jwt.verify(token,'123abc');
+		decoded=jwt.verify(token,process.env.JWT_SECRET);
 	}catch(e){
 		// return new Promise((resolve,reject)=>{
 		// 	reject();
